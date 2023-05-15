9b7fbf2bbcac331d42a59fe8b209b2fa84c0f8be
Adrian Manczak <manczakadrian@gmail.com>
2023-05-16 00:18:55 +0200

    add class+active_to_clicked_element

--- js/script.js
+++ js/script.js
@@ -25,27 +25,30 @@ const titleClickHandler = function (event) {
   }
 
   /* add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+  console.log('clickedElement (with plus): ' + clickedElement);
+
+
   console.log('clickedElement:', clickedElement);
 
   /* remove class 'active' from all articles */
-  
     console.log('Link was clicked!');
     console.log(event);
     const activeArticles = document.querySelectorAll('.post');
     for (let activeArticle of activeArticles) {
       activeArticle.classList.remove('active');
     }
-
+  }
     /* get 'href' attribute from the clicked link */
 
 
     /* find the correct article using the selector (value of 'href' attribute) */
 
     /* add class 'active' to the correct article????? */
-}
-const links = document.querySelectorAll('.titles a');
+
+  const links = document.querySelectorAll('.titles a');
     console.log(links);
     for (let link of links) {
-      link.addEventListener('click', titleClickHandler);
-      // activeArticles.classList.add('active');  //????
+      link.addEventListener('click', titleClickHandler);  
     }
+    
